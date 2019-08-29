9e2aadb102d2d72c7382da2eba6bb068959f3df4
iNex <x_akhmed_x@mail.ru>
2019-08-29 22:38:18 +0300

    test version

---
+++ VideoViewer/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.IO;
+using System.Threading;
+
+namespace VideoViewer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random randomVideo = new Random();
+
+        OptionsWindow optionsWindow;
+
+        SerialPort myPort;
+        int timer;
+
+        bool stateTimer = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myPort = new SerialPort();
+
+            playerMediaElement.Source = new Uri(AppDomain.CurrentDomain.BaseDirectory + @"Videos\0.mp4", UriKind.Absolute);
+
+            playerMediaElement.LoadedBehavior = MediaState.Manual;
+            playerMediaElement.UnloadedBehavior = MediaState.Manual;
+
+            playerMediaElement.Play();
+
+            string portName;
+            string baudRate;
+
+            using (StreamReader sr = new StreamReader("portOptions.txt", Encoding.Default))
+            {
+                portName = sr.ReadLine();
+                baudRate = sr.ReadLine();
+                timer = int.Parse(sr.ReadLine());
+            }
+
+            try
+            {
+                myPort.PortName = portName;
+                myPort.BaudRate = int.Parse(baudRate);
+                myPort.Open();
+
+                Thread threadTrueTimerState = new Thread(TestOnNewLog);
+                threadTrueTimerState.Start();
+
+                Thread threadFalseTimerState = new Thread(TestOnNewLogWhereTimer);
+                threadFalseTimerState.Start();
+            }
+            catch (Exception ex)
+            {
+                optionsWindow = new OptionsWindow();
+                optionsWindow.Show();
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+
+                this.Close();
+            }
+        }
+
+        void TestOnNewLog()
+        {
+            while (true)
+            {
+                if (myPort.ReadLine() != null && stateTimer)
+                {
+                    string message = myPort.ReadLine();
+                }
+            }
+        }
+
+        void TestOnNewLogWhereTimer()
+        {
+            while (true)
+            {
+                if (myPort.ReadLine() != null && !stateTimer)
+                {
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        playerMediaElement.Source = new Uri(AppDomain.CurrentDomain.BaseDirectory + @"Videos\" + randomVideo.Next(1, 21) + ".mp4", UriKind.Absolute);
+                        backroundTextBlock.Opacity = 100;
+                        playerMediaElement.Play();
+                        Thread.Sleep(500);
+                        backroundTextBlock.Opacity = 0;
+                    });
+
+                    stateTimer = true;
+                    Thread.Sleep(timer * 1000);
+                    stateTimer = false;
+                }
+            }
+        }
+
+        private void PlayerMediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            playerMediaElement.Source = new Uri(AppDomain.CurrentDomain.BaseDirectory + @"Videos\0.mp4", UriKind.Absolute);
+            backroundTextBlock.Opacity = 100;
+            playerMediaElement.Play();
+            Thread.Sleep(500);
+            backroundTextBlock.Opacity = 0;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.P)
+            {
+                optionsWindow = new OptionsWindow();
+                optionsWindow.Show();
+
+                this.Close();
+            }
+
+            if (e.Key == Key.Space)
+            {
+                playerMediaElement.Source = new Uri(AppDomain.CurrentDomain.BaseDirectory + @"Videos\" + randomVideo.Next(1, 21) + ".mp4", UriKind.Absolute);
+                backroundTextBlock.Opacity = 100;
+                playerMediaElement.Play();
+                Thread.Sleep(500);
+                backroundTextBlock.Opacity = 0;
+            }
+        }
+
+    }
+}
